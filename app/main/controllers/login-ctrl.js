2b508add34a3d7021937c397f78d0e231db946d4
Chad Furman <chad@chadfurman.com>
2015-09-06 13:50:46 -0400

    added login and thing controllers

---
+++ app/main/controllers/login-ctrl.js
@@ -0,0 +1,8 @@
+'use strict';
+angular.module('main')
+.controller('LoginCtrl', function () {
+
+  console.log('Hello from your Controller: LoginCtrl in module main:. This is your controller:', this);
+  // TODO: do your controller thing
+
+});
---
+++ app/main/controllers/thing-detail-ctrl.js
@@ -0,0 +1,8 @@
+'use strict';
+angular.module('main')
+.controller('ThingDetailCtrl', function () {
+
+  console.log('Hello from your Controller: ThingDetailCtrl in module main:. This is your controller:', this);
+  // TODO: do your controller thing
+
+});
